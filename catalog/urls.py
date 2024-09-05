4eedfeca812b4a27b6a9e8339a502f564ca3368f
xcheche <chekwubechukwu23@gmail.com>
2024-09-05 08:25:43 +0100

    views and urls

--- catalog/urls.py
+++ catalog/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("about/", views.about, name="about"),
+    path("book/<int:pk>/", views.BookDetailView.as_view(), name="book_detail"),
+    path("create/", views.CreateBook.as_view(), name="create_book"),
 ]
--- catalog/views.py
+++ catalog/views.py
@@ -1,11 +1,50 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, DetailView
+from .models import Book, Author, BookInstance, Genre, Language
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the catalog index.")
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+    num_instances_available = BookInstance.objects.filter(status__exact="a").count()
+    books = Book.objects.order_by('-id')
+    
+    context = {
+        "num_books": num_books,
+        "num_instances": num_instances,
+        "num_instances_available": num_instances_available,
+        "books": books
+    }
+
+    return render(request, "catalog/index.html", context=context)
 
 
-def about(request):
-    return HttpResponse("This is the about page.")
+
+
+#Detail view
+class BookDetailView(DetailView):
+    model = Book
+    template_name = "catalog/book_detail.html"
+    
+    
+    def get_context_data(self, **kwargs):
+        # Get the default context data from the parent class
+        context = super().get_context_data(**kwargs)
+
+        # Add the additional context data you want to include
+        context['num_books'] = Book.objects.all().count()
+        context['num_instances'] = BookInstance.objects.all().count()
+        context['num_instances_available'] = BookInstance.objects.filter(status__exact="a").count()
+
+        return context
+
+#Create View
+class CreateBook(CreateView):
+    model = Book
+    fields = "__all__"
+    template_name = "catalog/book_form.html"
+    success_url = reverse_lazy("index")
+    
+    
